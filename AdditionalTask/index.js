17d7ecc22652efafd8eb896f30cf7f7960245987
Bratok3000 <bratok3000@gmail.com>
2018-04-06 10:47:07 +0300

    forth commit

--- AdditionalTask/index.js
+++ AdditionalTask/index.js
@@ -4,16 +4,33 @@ window.onload = function () {
         var xhr2 = new XMLHttpRequest();
 
         xhr1.open("GET", "file1.html", true);
+        xhr2.open("GET", "file2.html", true);
 
+        // xhr1.onreadystatechange = function () {
+        //     if (xhr1.readyState == 4 && xhr1.status == 200) {
+        //         document.getElementById("output").innerHTML += xhr1.responseText;
+        //     }
+        //     if(xhr2.readyState ==4 && xhr2.status){
+        //         document.getElementById("output").innerHTML += xhr2.responseText;
+        //     }
+        // }
+        // xhr1.send();
+        // xhr2.send();
         xhr1.onreadystatechange = function () {
-            if (xhr1.readyState == 4 && xhr1.status == 200) {
-                document.getElementById("output").innerHTML += xhr1.responseText;
-            }
-            if(xhr2.readyState ==4 && xhr2.status){
-                document.getElementById("output").innerHTML += xhr2.responseText;
+            if (xhr1.readyState == 4) {
+                xhr2.onreadystatechange = function () {
+                    if (xhr2.readyState == 4) {
+                        if (xhr2.status == 200) {
+                            document.getElementById("output").innerHTML += xhr2.responseText;
+                        }
+                    }
+                }
+                xhr2.send();
+                if (xhr1.status == 200) {
+                    document.getElementById("output").innerHTML += xhr1.responseText;
+                }
             }
         }
         xhr1.send();
-        xhr2.send();
     }
 }
